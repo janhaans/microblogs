fcd2eafdd1c0342b7e05fdbedc28ec65bfcb5f46
Jan Haans <jan.haans@gmail.com>
2019-01-09 01:16:25 -0800

    Added first simple posts

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,11 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello world!'
+    user = {'username': 'Jan Haans'}
+    posts = [   {'author': 'Anna-Maria','body': 'Some text from Anna-Maria'},
+                {'author': 'Valentina', 'body': 'Some text from Valentina'}
+            ]
+    return render_template("index.html", user=user, posts=posts)
